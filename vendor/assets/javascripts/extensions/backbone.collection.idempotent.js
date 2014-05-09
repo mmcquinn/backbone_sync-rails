7f20b970fe69faca82c4c44c78f09c43191708eb
Michael McQuinn <mike@mcquinntechnologies.com>
2014-05-09 17:49:27 -0400

    In Backbone 1.0, collection.reset() adds undefined models.

--- vendor/assets/javascripts/extensions/backbone.collection.idempotent.js
+++ vendor/assets/javascripts/extensions/backbone.collection.idempotent.js
@@ -1,4 +1,6 @@
 Backbone.Collection.prototype._addWithIdCheck = function(model, options) {
+  if (model === undefined) return false;
+
   if ((!model || model.id === undefined || this.get(model.id) === undefined) || (options && options.merge)) {
     this._addWithoutIdCheck(model, options);
   }
